15c11e406cdd6f9f58859de5e75cb3fba67bbbe0
Unknown <luyhuy19@gmail.com>
2018-01-15 22:51:42 +0700

    connect to mongodb

---
+++ server/database.js
@@ -0,0 +1,9 @@
+var exports = module.exports = {};
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/achievement-book');
+var db = mongoose.connection;
+
+db.once('open', function () {
+    console.log('Connected to DB');
+})
