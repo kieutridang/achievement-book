5ca8ecc89614e13d0a0c2f848ff846d0e2b24db5
Unknown <luyhuy19@gmail.com>
2018-01-15 22:51:42 +0700

    connect to mongodb

--- server/router.js
+++ server/router.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+
 const staticPath = '../dist';
+var database = require('./database');
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, staticPath, '/index.html'))
