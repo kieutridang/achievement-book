38e4d7dfff16088ab133b89a67f2e506435debab
Nguyen Quang Thuc <zthucnguyen99@gmail.com>
2018-01-16 21:04:46 +0700

    separate router.js

---
+++ server/express.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+const staticPath = '../dist';
+var database = require('./database');
+
+app.get(/^\/[a-z]*$/, (req, res) => {
+    res.sendFile(path.join(__dirname, staticPath, '/index.html'))
+})
+
+app.use('/', express.static(path.join(__dirname, staticPath)))
+
+app.listen(8080, () => { console.log('Server running on port 8080') });
