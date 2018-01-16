ca56c9b6b4efe99ed5918161d99c591127d7febe
Nguyen Quang Thuc <zthucnguyen99@gmail.com>
2018-01-16 14:51:53 +0700

    test model

--- server/database.js
+++ server/database.js
@@ -1,9 +1,18 @@
-var exports = module.exports = {};
-var mongoose = require('mongoose');
+var exports = module.exports = {}
+var mongoose = require('mongoose')
+var UserInfo = require('./models/user')(db)
 
 mongoose.connect('mongodb://localhost/achievement-book');
 var db = mongoose.connection;
 
 db.once('open', function () {
     console.log('Connected to DB');
+    var user1 = new UserInfo({
+        username: "zigonk",
+        password: "abcdef",
+        fullname: "Nguyen Quang Thuc",
+        email: "zthucnguyen99@gmail.com",
+        DOB: 24-03-1999,
+        gender: "Male"
+    })
 })
