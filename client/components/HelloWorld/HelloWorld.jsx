7cfa18d5c5c5238befd8b67c262c4b4724e2cfcf
Duy Mai <duymai1612@gmail.com>
2018-01-16 11:38:34 +0700

    002: Auto stash before rebase of "origin/develop"

--- client/components/HelloWorld/HelloWorld.jsx
+++ client/components/HelloWorld/HelloWorld.jsx
@@ -3,12 +3,23 @@ import Button from '../Button/index.jsx'
 
 
 export default class HelloWord extends React.Component {
-    state = {  }
+    constructor(props) {
+        super(props);
+        this.state = {  }
+        this.alertHelloWorld = this.alertHelloWorld.bind(this);
+    }
+
+    alertHelloWorld() {
+        alert('Hello World!!');
+    }
+
     render() {
         return (
             <div>
                 <p>Hello World</p>
-                <Button name='Hello'></Button>
+                <Button 
+                name='Hello'
+                handleClick={() => this.alertHelloWorld()}></Button>
             </div>
         );
     }
