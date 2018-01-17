c50636cab8b82154d53ae1e78cf3c17eab58e1c3
Nguyen Quang Thuc <zthucnguyen99@gmail.com>
2018-01-17 22:32:44 +0700

    change from forever to pm2

--- server/express.js
+++ server/express.js
@@ -5,10 +5,14 @@ const app = express()
 const staticPath = '../dist';
 var database = require('./database');
 
+app.get('/abc', (req, res) => {
+    res.end('Hello');
+})
+
 app.get(/^\/[a-z]*$/, (req, res) => {
     res.sendFile(path.join(__dirname, staticPath, '/index.html'))
 })
 
 app.use('/', express.static(path.join(__dirname, staticPath)))
 
-app.listen(8080, () => { console.log('Server running on port 8080') });
+app.listen(8080, () => { console.log('Server running on port 8080')});
