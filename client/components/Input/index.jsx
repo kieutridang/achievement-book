ad6acc772f86d28f02b1267860b069a20bb0d564
Nguyen Quang Thuc <zthucnguyen99@gmail.com>
2018-01-19 21:04:33 +0700

    solve conflict

--- client/components/Input/index.jsx
+++ client/components/Input/index.jsx
@@ -2,49 +2,15 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 export default class Input extends Component {
-    constructor(props) {
-      super(props);
-      this.state ={
-        message:"This field can't be empty",
-        validate: false,
-      }
-    }
-    render() {
-      const {label, property, validateAndMessage, type, changeHandle, validate, showValidation} = this.props;
-      return (
-        <div className = 'Input'>
-          <label>{label} :</label>
-          <input
-            value = {this.props.value}
-            type  = {type}
-            onChange = {this._changeHandle.bind(this, label)}
-            onBlur = {this._changeHandle.bind(this, label)}
-          />
-          <div></div>
-          {showValidation && !this.state.validate && <label>{this.state.message}</label>}
-        </div>
-      );
-    }
-    _changeHandle = (label, e) => {
-      let newValue = e.target.value;
-      let newMessage = "";
-      let newValidate = true;
-      newValue = newValue.split(" ").filter(function(c) {return c!=""}).join(' ');
-      for (let i = 0; i < this.props.validateAndMessage.length; ++i) {
-        let element = this.props.validateAndMessage[i];
-        let checkRegExp = new RegExp(element.regExp);
-        if (checkRegExp.test(newValue)) {
-          newMessage = element.message;
-          newValidate = element.valid;
-          break;
-        }
-      }
-      this.setState({
-        value: e.target.value,
-        message: newMessage,
-        validate: newValidate
-      }, () => {
-        this.props.changeHandle(this.props.property, this.props.value, this.state.validate);
-      });
+  constructor(props){
+    super(props);
+    this.state ={
+      
     }
   }
+  render() {
+    return (
+      <input/>
+    )
+  }
+}
