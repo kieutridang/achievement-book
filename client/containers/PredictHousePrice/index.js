924e9513ecb8f5d480538ae596c91b038afa253f
Kiều Trí Đăng <dang.kieu@8bitrockr.com>
2018-01-21 16:05:05 +0700

    complete ui for predict page

--- client/containers/PredictHousePrice/index.js
+++ client/containers/PredictHousePrice/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import TrainingDataTable from '../TrainingDataTable/index'
+import PredictBox from '../PredictBox/index'
 
 export default class PredictHousePrice extends Component {
   state = {  }
   render() {
     return (
       <div>
+        <PredictBox/>
         <TrainingDataTable/>
       </div>
     );
