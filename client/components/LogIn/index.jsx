1d73b9a290585d8f37e56ab0fbe210660ddce542
Duy Mai <duymai1612@gmail.com>
2018-01-22 18:23:41 +0700

    continue coding

---
+++ client/components/LogIn/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import Input from '../Input/index.jsx'
+import Button from '../Button/index.jsx'
+
+export default class LogIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1> Log In </h1>
+      </div>
+    )
+  }
+}
