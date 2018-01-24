0f33404926437ffd379cf080178113cdb7d04acb
zigonk <zthucnguyen99@gmail.com>
2018-01-24 17:35:27 +0700

    Add deleteUser

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,5 +1,3 @@
-import { text } from '../../../../../.cache/typescript/2.6/node_modules/@types/body-parser';
-
 var mongoose = require('mongoose')
 var dataUser = require('./data_user')
 
@@ -9,8 +7,7 @@ var db = mongoose.connection
 exports.findUser = function(req, res) {
   try {
     debugger
-    dataUser.findUser(db, req.params, function (data) {
-      debugger
+    dataUser.findUser(db, res, req.params, function (data) {
       res.status(200).send(JSON.stringify(data));
     })
   } catch (ex) {
@@ -28,7 +25,7 @@ exports.createUser = function(req, res) {
       }
       else {
         dataUser.createUser(db, newUser);
-        res.status(200).end();
+        res.status(200).end('Create user successful');
       }
     })
   } catch(ex) {
@@ -39,18 +36,26 @@ exports.createUser = function(req, res) {
 exports.updateUser = function(req, res) {
   try {
     var newUser = req.body;
-    if (newUser.username) {
-      res.status(403).send("You have no permission to change username")
+    if (newUser.username || newUser.password) {
+      res.status(403).send("You have no permission to change username or password")
       res.end();
       return;
     }
-    dataUser.updateUser(db, req.params, newUser, function(err, data) {
-      if (err) {
-        console.log(err)
-        return;
-      }
-      res.status(200).send("Update successfuly");
-      res.end();
+    else {
+      dataUser.updateUser(db, res, req.params._id, newUser, function(err, data) {
+        res.status(200).send("Update succeeded");
+        res.end();
+      })
+    }
+  } catch (ex) {
+    res.status(500).send(ex)
+  }
+}
+
+exports.deleteUser = function(req, res) {
+  try {
+    dataUser.deleteUser(db, res, req.params._id, (data) => {
+      res.status(200).send('Delete succeeded')
     })
   } catch (ex) {
     res.status(500).send(ex)
