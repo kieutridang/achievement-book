6e522215ba661907a05e094169d096ed68c33c8f
zigonk <zthucnguyen99@gmail.com>
2018-01-24 17:35:27 +0700

    Add deleteUser

--- server/controller/data_user.js
+++ server/controller/data_user.js
@@ -1,29 +1,40 @@
 var mongoose = require('mongoose')
 
-exports.findUser = function(db, data, callback) {
+exports.findUser = function(db, res, data, callback) {
   var User = require('../models/user')(db)
   User.find(data, function(err, data){
-      if (err) return console.log(err);
+      if (err) return res.status(500).send(err);
       callback(data);
   })
 }
 
-exports.createUser = function(db, data) {
+exports.createUser = function(db, res, data) {
   var User = require('../models/user')(db)
   var newUser = new User(data)
   newUser.save(function(err, data) {
-    if (err) console.log(err)
+    if (err) res.status(500).send(err)
     else {
-      User.find(data, function(err, data){
-        console.log(data);
-      })
+      console.log(data)
     }
   })
 }
 
-exports.updateUser = function(db, condition, data, callback) {
+exports.updateUser = function(db, res, condition, data, callback) {
   var User = require('../models/user')(db)
   User.findOneAndUpdate(condition, data, {$new: false}, function(err, model) {
-    callback(err, model)
+    if (err) res.status(500).send(err) 
+    else {
+      callback(model)
+    }
+  })
+}
+
+exports.deleteUser = function(db, res, condition, callback) {
+  var User = require('../models/user')(db)
+  User.findByIdAndRemove(condition, (err, data) => {
+    if (err) res.status(500).send(err) 
+    else {
+      callback(data)
+    }
   })
 }
