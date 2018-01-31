ce1f31f753c4be543c9fc2ef4ea58738ec989c05
Kiều Trí Đăng <dang.kieu@8bitrockr.com>
2018-01-31 21:21:58 +0700

    remove the remain thing

--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
-import routes from './router.jsx'
 import Board from './components/Board/index.jsx'
 import { observe } from './components/functions/game'
 
