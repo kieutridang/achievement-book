d2976f5f39142ff6d39d5c4fcbe51752b45f4262
Nguyễn Ngọc Minh Huy <luyhuy19@gmail.com>
2018-02-01 13:00:26 +0700

    app

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-const express = require('express')
+const express = require('express');
+var session = require('express-session');
 const app = express()
+
+app.use(session(
+    module.exports = {
+        secret: 'achievement-book',
+        resave: false,
+        saveUninitialized: false,
+        httpOnly: true,
+        cookie: {
+            maxAge: 600000
+        }
+    }
+))
 
 require('./server/express')(app)
 require('./server/router')(app)
