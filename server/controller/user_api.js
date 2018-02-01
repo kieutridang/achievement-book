1348986438d65546efc0e59b796c86ec58583e84
Nguyễn Ngọc Minh Huy <luyhuy19@gmail.com>
2018-02-01 12:50:23 +0700

    user_api

---
+++ server/controller/user_api.js
@@ -0,0 +1,80 @@
+var dataUser = require('./data_user');
+
+module.exports = {
+    login: function(req, res){
+        try {
+            var { username, password } = req.body;
+            dataUser.findUser({ username: username }, function (err, data) {
+                if (err) {
+                    res.status(500).end(err);
+                }
+                else {
+                    if (data.length <= 0) {
+                        res.status(404).end('User not found');
+                    }
+                    else {
+                        var User = require('../models/user');
+                        var user = new User(data[0]);
+                        user.verifyPassword(password, function(err, valid){
+                            if (err) {
+                                res.status(500).end(err);
+                            }
+                            if (valid) {
+                                req.session.regenerate(function (err) {
+                                    if (err) {
+                                        res.status(500).end(err);
+                                    }
+                                    else {
+                                        req.session.user = {
+                                            username: user.username
+                                        }
+                                    }
+                                })
+                                res.status(200).end('Logged in successfully');
+                            }
+                            else {
+                                res.end(401).end('Incorrect password');
+                            }
+                        })
+                    }
+                }
+            })
+        } catch (err) {
+            res.status(500).end(err);
+        }
+        
+    },
+
+    logout: function(req, res){
+        try {
+            req.session.destroy(function(err){
+                if (err) {
+                    res.status(500).end(err);
+                }
+                else {
+                    res.status(200).end('Logged out successfully');
+                }
+            })
+        } catch (error) {
+            
+        }
+    },
+
+    athenticate: function(req, res, next){
+        if (req.session.user){
+            next();
+        }
+        else {
+            res.status(401).end('No permission, must login');
+        }
+    },
+
+    checkAuthenticate: function(req, res){
+        if (req.session.user){
+            res.status(200).end('Logged in');
+        }
+        else {
+            res.status(401).end('Not logged in');
+        }
+    }
+}
--- server/router.js
+++ server/router.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const user = require('./controller/user')
-
+const express = require('express');
+const user = require('./controller/user');
+const userApi = require('./controller/user_api');
 module.exports = function(app) {
     var router = express.Router()
     
     router.get('/user/getuser/userid/:_id', user.findUser)
     router.post('/user/createuser', user.createUser)
     router.put('/user/updateuser/userid/:_id', user.updateUser)
+    router.post('/user/login', userApi.login);
+    router.get('/user/checkAuthenticate', userApi.checkAuthenticate);
     app.use('/api', router)
 }
