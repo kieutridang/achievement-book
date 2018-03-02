ece8ceba58265015e2ba7114c996c04ad869bb92
Duy Mai <duymai1612@gmail.com>
2018-03-02 20:11:00 +0700

    getDailyPlan

--- client/containers/DayStart/index.jsx
+++ client/containers/DayStart/index.jsx
@@ -3,6 +3,7 @@ import OnBlurInput from '../../components/OnBlurInput/index.jsx'
 import Table from '../../components/Table/index.jsx'
 
 import { Link } from 'react-router-dom'
+import { _helper } from '../../components/api/_helper'
 
 export default class DailyPlan extends Component {
   constructor(props) {
@@ -15,6 +16,26 @@ export default class DailyPlan extends Component {
     }
   }
 
+  getDailyPlan = () => {
+    const { date } = this.state;
+    _helper.fetchGET(
+      '/dailyplan/getplan/:' + date,
+      {}
+    )
+    .then((response) => {
+      const { date, quote, plan, note } = response;
+      return this.setState({
+        quote: quote,
+        plan: plan,
+        note: note
+      })
+    })
+  }
+
+  componentWillMount = () => {
+    this.getDailyPlan();
+  }
+
   render() {
     const { date, quote, plan, note } = this.state
     return (
@@ -24,9 +45,15 @@ export default class DailyPlan extends Component {
           <div>
             <input type="image" src="../../../public/backward.png" alt="Back" width="48" height="48"/>
             <OnBlurInput
+              default={date}
               type = 'date'
               label = 'Date'
-              onBlur = {date => this.setState({date})}
+              onBlur = {date => {
+                this.setState(
+                  {date},
+                  this.getDailyPlan()
+                )
+              }}
             />
             <input type="image" src="../../../public/forward.png" alt="Next" width="48" height="48"/>
           </div>
