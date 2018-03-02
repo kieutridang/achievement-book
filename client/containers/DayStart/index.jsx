f63d4b4a925063afdb5d060a744340391f746a6c
Duy Mai <duymai1612@gmail.com>
2018-03-02 20:33:59 +0700

    finish DailyPlan

--- client/containers/DayStart/index.jsx
+++ client/containers/DayStart/index.jsx
@@ -5,11 +5,13 @@ import Table from '../../components/Table/index.jsx'
 import { Link } from 'react-router-dom'
 import { _helper } from '../../components/api/_helper'
 
+import moment from 'moment'
+
 export default class DailyPlan extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      date: '',
+      date: new Date().moment().format('YYYY-MM-DD'),
       quote: '',
       plan: [],
       note: ''
@@ -62,12 +64,16 @@ export default class DailyPlan extends Component {
         <div>
           <Table
             label='Tasks Planning'
+            reqUrl={'/dailyplan/updateplan/:' + {date}}
           />
         </div>
         <div>
           <OnBlurInput
             label = 'Note'
-            onBlur = {note => this.setState({note})}
+            onBlur = {note => this.setState(
+              {note},
+              _helper.fetchAPI('/dailyplan/updateplan/:' + {date}, {note: note}, [], 'PUT')
+            )}
           />
         </div>
         <div>
