784feb1764cf4713e7e8bb312d8bbf305a42310d
Nguyễn Ngọc Minh Huy <luyhuy19@gmail.com>
2018-03-13 09:22:20 +0700

    css for container

--- client/index.jsx
+++ client/index.jsx
@@ -3,6 +3,8 @@ import ReactDom from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import routes from './router.jsx'
 
+import './index.scss'
+
 ReactDom.render((
     <BrowserRouter>
         {routes}
