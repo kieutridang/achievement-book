d0584a698c9d28535635ffcf43dede6e491fb6c2
Nguyễn Ngọc Minh Huy <luyhuy19@gmail.com>
2018-03-14 20:03:36 +0700

    error with responsive

--- client/components/Input2/index.jsx
+++ client/components/Input2/index.jsx
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
 
 export default class Input extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+  constructor(props) {
+    super(props);
+    this.state = {
 
-        }
     }
-    render() {
-        let { type, label, onChange, message, showMessage } = this.props;
-        return (
-            <div>
-                <input
-                    type={type || 'text'}
-                    onChange={(e) => (onChange(e.target.value))}
-                    placeholder={label}
-                />
-                {showMessage && message && <span>{message}</span>}
-            </div>
-        );
-    }
+  }
+  render() {
+    let { type, label, onChange, message, showMessage } = this.props;
+    return (
+      <div>
+        <input
+          type={type || 'text'}
+          onChange={(e) => (onChange(e.target.value))}
+          placeholder={label}
+        />
+        {showMessage && message && <span>{message}</span>}
+      </div>
+    );
+  }
 }
