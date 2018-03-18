de23479c8b7ef9d9279934e59a0438b826c78adf
zigonk <zthucnguyen99@gmail.com>
2018-03-18 22:14:57 +0700

    Move task automatically

--- server/controller/data_daily_plan.js
+++ server/controller/data_daily_plan.js
@@ -1,32 +1,32 @@
 var mongoose = require('mongoose');
 
 module.exports = {
-    findPlan: function(data, callback){
-        var Daily = require('../models/daily_plan');
-        Daily.find(data, function(err, data){
-            callback(err, data);
-        })
-    },
+	findPlan: function (data, callback) {
+		var Daily = require('../models/daily_plan');
+		Daily.find(data, function (err, data) {
+			if (callback) callback(err, data);
+		})
+	},
 
-    createPlan: function(data, callback){
-        var Daily = require('../models/daily_plan');
-        var newPlan = new Daily(data);
-        newPlan.save(function(err, newData){
-            callback(err, newData);
-        })
-    },
+	createPlan: function (data, callback) {
+		var Daily = require('../models/daily_plan');
+		var newPlan = new Daily(data);
+		newPlan.save(function (err, newData) {
+			if (callback) callback(err, newData);
+		})
+	},
 
-    updatePlan: function(condition, data, callback){
-        var Daily = require('../models/daily_plan');
-        Daily.findOneAndUpdate(condition, data, {new: false, upsert: true, runValidators: true}, function(err, model){
-            callback(err, model);
-        })
-    },
+	updatePlan: function (condition, data, callback) {
+		var Daily = require('../models/daily_plan');
+		Daily.findOneAndUpdate(condition, data, { new: false, upsert: true, runValidators: true }, function (err, model) {
+			if (callback) callback(err, model);
+		})
+	},
 
-    deletePlan: function(condition, callback) {
-        var Daily = require('../models/daily_plan');
-        Daily.findByIdAndRemove(condition, function(err, data){
-            callback(err, data);
-        })
-    }
+	deletePlan: function (condition, callback) {
+		var Daily = require('../models/daily_plan');
+		Daily.findByIdAndRemove(condition, function (err, data) {
+			if (callback) callback(err, data);
+		})
+	}
 }
