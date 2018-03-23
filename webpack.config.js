0686b3acf7a4284f9ddee8f4704e0feb1bee1c24
Duy Mai <duymai1612@gmail.com>
2018-03-23 20:54:43 +0700

    add url-loader, edit css

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,8 @@ module.exports = {
                     }],
                     fallback: "style-loader"
                 })
-            }
+            },
+            { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
         ]
     },
     devServer: {
