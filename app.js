514c857711bbd1eb81016de0b3420efd40b7ca86
Duy Mai <duymai1612@gmail.com>
2018-03-26 23:28:48 +0700

    add process.env.PORT

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const app = express();
 require('./server/express')(app);
 require('./server/router')(app);
 
-app.listen(8080, () => { console.log('Server running on port 8080')});
+app.listen(process.env.PORT || 8080, () => { console.log('Server running on port 8080')});
