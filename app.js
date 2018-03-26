865b0dcaca4dc83d590504fb7a8a9fb7adba748f
zigonk <zthucnguyen99@gmail.com>
2018-03-26 22:07:45 +0700

    config deploy

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 5000
 
 require('./server/express')(app);
 require('./server/router')(app);
 
-app.listen(8080, () => { console.log('Server running on port 8080')});
+app.listen(PORT, () => { console.log('Server running on port 8080')});
