357c208c1399c91c96b6187fce3488ba7a62c2a4
zigonk <zthucnguyen99@gmail.com>
2018-03-27 20:29:01 +0700

    fix bug when dont have today plan

--- server/controller/helper.js
+++ server/controller/helper.js
@@ -6,10 +6,15 @@ module.exports.moveTaskAutomatically = function () {
   User.findUser({}, (err, listUser) => {
     listUser.forEach(userInfo => {
       let today = moment().format('YYYY-MM-DD');
+
       DailyPlan.findPlan({ userId: userInfo._id, date: today }, (err, todayPlanData) => {
+
+        if (todayPlanData.length == 0) return;
         todayPlanData = todayPlanData[0];
         let tommorow = moment().add(1, 'd').format('YYYY-MM-DD');
+
         DailyPlan.findPlan({ userId: userInfo._id, date: tommorow }, (err, tommorowPlanData) => {
+
           if (tommorowPlanData.length == 0) {
             DailyPlan.createPlan({ userId: userInfo._id, date: tommorow }, (err, tommorowPlanData) => {
               let incompletePlan = todayPlanData.plan.filter(plan => {
