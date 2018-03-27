bb0d3a416cab5ddae9be611cab53341cf2711c6a
Duy Mai <duymai1612@gmail.com>
2018-03-27 21:56:24 +0700

    full width tickbar, add responsive

--- client/components/SingleChoice/index.jsx
+++ client/components/SingleChoice/index.jsx
@@ -17,27 +17,16 @@ export default class SingleChoice extends Component {
             optionsList.map((option, index) => {
               return (
                 <div key={index}>
-                  { index === choice ? 
-                    <label className="radio-image chosen-radio" for={option}>
-                      <input
-                        type='radio'
-                        name={property}
-                        value={option}
-                        id={option}
-                        checked
-                        onChange={(e) => this.props.onChange(e.target.value)} 
-                      />
-                    </label>
-                  : <label className="radio-image" for={option}>
-                      <input
-                        type='radio'
-                        name={property}
-                        value={option}
-                        id={option}
-                        onChange={(e) => this.props.onChange(e.target.value)} 
-                      />
-                    </label>
-                  }
+                  <label className={choice === index ? "radio-image chosen-radio" : "radio-image"} htmlFor={option}>
+                    <input
+                      type='radio'
+                      name={property}
+                      value={option}
+                      id={option}
+                      checked={index === choice}
+                      onChange={(e) => this.props.onChange(e.target.value)} 
+                    />
+                  </label>
                   <label>
                     {option} 
                     <span></span>
