9375ec01dcaf9150fcf8003e0a5308d77e08f082
zigonk <zthucnguyen99@gmail.com>
2018-03-28 22:05:01 +0700

    fix url

--- client/components/api/_helper.js
+++ client/components/api/_helper.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = (process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:8080/api') ;
+const baseURL = (process.env.NODE_ENV == 'production' ? '/api' : 'http://localhost:8080/api') ;
 
 const defaultHeaders = [{ 'Content-Type': 'javascript/json' }]
 
