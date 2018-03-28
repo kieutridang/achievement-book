1d8b94c25d20dc065282b284332c3bbc8534c141
zigonk <zthucnguyen99@gmail.com>
2018-03-28 19:52:36 +0700

    fix deploy port and Content Security Policy

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 
 require('./server/express')(app);
 require('./server/router')(app);
