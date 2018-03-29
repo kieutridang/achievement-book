94c8528f84819babcf91d66b6e7da157d235bae5
Duy Mai <duymai1612@gmail.com>
2018-03-29 21:15:42 +0700

    edit singleChoice like dev branch

--- client/components/SingleChoice/index.jsx
+++ client/components/SingleChoice/index.jsx
@@ -10,27 +10,30 @@ export default class SingleChoice extends Component {
     const property = label.toLowerCase().replace(' ', '-');
     return (
       <div>
-        <label className='page-label'> {label}: </label>
+        <label> {label}: </label>
         <form>
           { required && <span> * </span> }
           {
             optionsList.map((option, index) => {
               return (
                 <div key={index}>
-                  <label className={choice === index ? "radio-image chosen-radio" : "radio-image"} htmlFor={option}>
-                    <input
-                      type='radio'
-                      name={property}
-                      value={option}
-                      id={option}
-                      checked={index === choice}
-                      onChange={(e) => this.props.onChange(e.target.value)} 
-                    />
-                  </label>
                   <label>
-                    {option} 
+                    {
+                      index === choice ? <input
+                        type='radio'
+                        name={property}
+                        value={option}
+                        checked
+                        onChange={(e) => this.props.onChange(e.target.value)}/>
+                      : <input
+                        type='radio'
+                        name={property}
+                        value={option}
+                        onChange={(e) => this.props.onChange(e.target.value)}/>
+                    }
                     <span></span>
                   </label>
+                  {option}
                 </div>
               )
             })
