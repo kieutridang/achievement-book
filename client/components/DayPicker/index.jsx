e62410bb9f3b7b4db548793c2223f1a43c5a9a22
Nguyễn Ngọc Minh Huy <luyhuy19@gmail.com>
2018-04-06 18:26:57 +0700

    day, week, month picker

---
+++ client/components/DayPicker/index.jsx
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import moment from 'moment';
+import 'react-dates/initialize';
+
+import { DayPickerSingleDateController } from 'react-dates';
+
+import 'react-dates/lib/css/_datepicker.css';
+
+export default class DayPicker extends Component {
+    state = {  }
+
+    onDateChange = (date) => {
+        this.props.handleSelect(date);
+    }
+
+    onPrevMonthClick = () => {
+        const { date } = this.props;
+        this.props.handleSelect(moment(date.toDate()).subtract(1, 'month'));
+    }
+
+    onNextMonthClick = () => {
+        const { date } = this.props;
+        this.props.handleSelect(moment(date.toDate()).add(1, 'month'));
+    }
+
+    render() {
+        debugger
+        const { date } = this.props;
+        return (
+            <DayPickerSingleDateController
+                onDateChange={this.onDateChange}
+                date={date}
+                enableOutsideDays={true}
+            />
+        );
+    }
+}
