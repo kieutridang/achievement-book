001f413f6e84dade279898f8368f0f1769b3707e
Nguyễn Ngọc Minh Huy <luyhuy19@gmail.com>
2018-04-17 21:47:58 +0700

    basic week end page

--- client/containers/WeekEnd/index.jsx
+++ client/containers/WeekEnd/index.jsx
@@ -1,4 +1,190 @@
 import React, { Component } from 'react';
+import moment from 'moment';
+import BlockUi from 'react-block-ui';
+import 'react-block-ui/style.css';
+
+import SideBar from '../../components/SideBar/index.jsx';
+import NavigationBar from '../../components/NavigationBar/index.jsx';
+import Select from '../../components/Select/index.jsx';
+import EditableP from '../../components/EditableP/index.jsx';
+import TickBar from '../../components/TickBar/index.jsx';
+import SingleChoice2 from '../../components/SingleChoice2/index.jsx';
+import SlideTab from '../../components/SlideTab/index.jsx';
+
+import { _helper } from '../../components/api/_helper';
+import checkAuthenticate from '../../components/functions/checkAuthenticate';
+
+export default class WeekEnd extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: props.match.params.date || moment().weekday(0).format('YYYY-MM-DD'),
+      user: {},
+      blockingUI: false,
+      completedTasksList: ['abc', 'cde'],
+      mostEnthusiasticDays: [],
+      efficiency: 'Low',
+      experience: [{
+        Problem: 'Duy',
+        Cause: 'Huy',
+        Solution: 'Thuc',
+      }],
+    }
+  }
+
+  checkAuth = () => {
+    const { history } = this.props;
+    checkAuthenticate().then((authenticate) => {
+      if (!authenticate) {
+        history.replace('/users/login');
+      }
+    })
+  }
+
+  getUser() {
+    _helper.fetchGET(
+      '/user/getuser',
+      {}
+    )
+      .then((response) => {
+        const { data, status } = response;
+        if (status == 200) {
+          this.setState({ user: data })
+        }
+      })
+  }
+
+  handleDateChange = (date) => {
+    const { history } = this.props;
+    history.push({ pathname: '/week-result/' + date });
+  }
+
+  componentDidMount = () => {
+    this.checkAuth();
+    this.getUser()
+  }
+
+  render() {
+    const { date, rateOfFinishing, completedTasksList, bestTask, lessonLearned, mostEnthusiasticDays, taskRating, experience } = this.state;
+    return (
+      <BlockUi tag="div" blocking={this.state.blockingUI} message="Please wait" keepInView>
+        <NavigationBar user={this.state.user}
+          date={date}
+          type={1}
+          handleDateChange={this.handleDateChange}
+          page='result'
+        />
+        <div>
+          <SideBar
+            date={date}
+            type={1}
+            handleDateChange={this.handleDateChange}
+            page='result'
+          />
+          <div>
+            <div>
+              <h1>Review your week</h1>
+            </div>
+            <div>
+              <div>
+                <span>Rate of finishing task: </span>
+                <span>{rateOfFinishing} %</span>
+              </div>
+              <Select
+                label='Which completed task you feel best?'
+                optionsList={completedTasksList}
+                selectedValue={bestTask}
+                disabled={completedTasksList && completedTasksList.length == 0}
+                disabledMessage="You haven't done any task"
+                onChange={(bestTask) => this.setState(
+                  { bestTask },
+                  () => {
+                  }
+                )}
+              />
+              <div>
+                <span>What have you learned through this week?</span>
+                <EditableP
+                  defaultValue={lessonLearned}
+                  handleChange={lessonLearned => this.setState(
+                    { lessonLearned },
+                    () => {
+
+                    }
+                  )}
+                  maxlength={200}
+                />
+              </div>
+              <div>
+                <label className='page-label'>Best day(s) in week: </label>
+                <TickBar
+                  label={null}
+                  selections={['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sar']}
+                  selected={mostEnthusiasticDays}
+                  onChange={mostEnthusiasticDays => this.setState(
+                    { mostEnthusiasticDays },
+                    () => {
+
+                    }
+                  )}
+                />
+              </div>
+              <SingleChoice2
+                choice={taskRating}
+                label='How do you feel about this week'
+                optionsList={['Terrible', 'Normal', 'Good', 'Interest', 'Wonderful']}
+                onChange={(taskRating) => {
+                  var taskRatingNumber;
+                  switch (taskRating) {
+                    case 'Terrible':
+                      taskRatingNumber = 0;
+                      break;
+                    case 'Normal':
+                      taskRatingNumber = 1;
+                      break;
+                    case 'Good':
+                      taskRatingNumber = 2;
+                      break;
+                    case 'Interest':
+                      taskRatingNumber = 3;
+                      break;
+
+                    default:
+                      taskRatingNumber = 4;
+                      break;
+                  }
+                  this.setState(
+                    { taskRating: taskRatingNumber },
+                    () => {
+
+                    }
+                  )
+                }}
+              />
+            </div>
+            <span>Problem - Cause - Solution</span>
+            {
+              experience.map((data, index) =>
+                <SlideTab
+                  key={index}
+                  data={data}
+                  handleChange={(value, label) => {
+                    let newExperience = [];
+                    experience.forEach((data, index) => {newExperience[index] = data})
+                    newExperience[index][label] = value;
+                    this.setState({
+                      experience: newExperience,
+                    })
+                  }}
+                />)
+            }
+          </div>
+        </div>
+      </BlockUi>
+    );
+  }
+}
+import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 import { compose } from 'redux';
--- client/router.jsx
+++ client/router.jsx
@@ -7,7 +7,6 @@ import SignUp from './containers/SignUp/index.jsx';
 import Home from "./containers/Home/index.jsx";
 import DailyPlan from './containers/DayStart/index.jsx';
 import DailyResult from './containers/DayEnd/index.jsx';
-import WeeklyPlan from './containers/WeekStart/index'
 import WeeklyResult from './containers/WeekEnd/index.jsx'
 
 const routes = (
@@ -20,8 +19,9 @@ const routes = (
         <Route exact path='/day-plan/:date' component={DailyPlan} />        
         <Route exact path='/day-result' component={DailyResult} />
         <Route exact path='/day-result/:date' component={DailyResult} />
+        <Route exact path='/week-result/' component={WeekEnd} />
+        <Route exact path='/week-result/:date' component={WeekEnd} />
         <Route exact path='/week-plan' component={WeeklyPlan} />
-        <Route exact path='/week-result' component={WeeklyResult} />
     </Switch>
 )
 
