45cbcdda7b7ed4f1243df1c06b2d53904bdb2770
Nguyễn Ngọc Minh Huy <luyhuy19@gmail.com>
2018-04-17 19:55:34 +0700

    SlideTab component

--- client/components/App/index.jsx
+++ client/components/App/index.jsx
@@ -12,6 +12,7 @@ import MonthPicker from '../MonthPicker/index.jsx';
 import WeekPicker from '../WeekPicker/index.jsx';
 import DayPicker from '../DayPicker/index.jsx';
 import DatePicker from '../DatePicker/index.jsx';
+import SlideTab from '../SlideTab/index.jsx';
 
 import 'react-dates/lib/css/_datepicker.css';
 // import './index.scss';
@@ -33,112 +34,33 @@ export default class App extends Component {
     super(props);
     this.state = {
       date: moment().weekday(0),
-      focusedInput: END_DATE,
-      startDate: moment(),
-      endDate: moment(),
-      name: '',
-      gender: '',
-      city: 'Ho Chi Minh',
-      department: [],
-
-      showMessage: false,
-      date: moment().format('YYYY-MM-DD'),
-      url: ''
+      data: {
+        Problem: 'Duy',
+        Cause: 'Huy',
+        Solution: 'Thuc',
+      },
     }
   }
-  getURL = () => {
-    const newUrl = window.location.href.split("/")[window.location.href.split("/").length-1];
-    this.setState({url: newUrl});
-  }
 
-  onDatesChange = ({startDate, endDate}) => {
-    this.setState({ startDate, endDate });
-  }
-
-  onFocusChange = (focusedInput) => {
+  handleChange = (value, label) => {
+    let newData = {};
+    const { data } = this.state;
+    Object.keys(this.state.data).forEach((key) => {newData[key] = data[key]})
+    newData[label] = value;
     this.setState({
-      focusedInput: !focusedInput ? START_DATE : focusedInput,
-    });
-  }
-
-  handleSelect = (date) => {
-    this.setState({
-      date: date
+      data: newData,
     })
   }
-  componentDidMount = () => {   
-    this.getURL();
-  }
-  componentDidMount = () => {   
-    this.getURL();
-  }
-  render() {
 
+  render() {
+    const { data } = this.state;
     return (
       <div>
         <DatePicker/>
-        {/* <MonthPicker
-          date={this.state.date}
-          handleSelect={this.handleSelect}/>
-        <NavigationBar url={this.state.url}/>
-        <button onClick={() => {
-          _helper.fetchGET(
-            '/user/userid/5a61f703f29dd3283e449c96',
-            [{ 'Content-Type': 'javascript/json' }], )
-            .then((response) => {
-            })
-        }}>
-          ClickMe
-        </button>
-        <Select
-          label='City'
-          required={true}
-          optionsList={[
-            'Ho Chi Minh',
-            'Ha Noi',
-            'Da Nang'
-          ]}
-          onChange={(city) => this.setState({ city })} />
-        <SingleChoice
-          label='Gender'
-          required={true}
-          optionsList={[
-            'Male',
-            'Female'
-          ]}
-          message={
-            checkValidate.checkSingleChoice(this.state.gender, true, validations.gender)
-          }
-          showMessage={this.state.showMessage}
-          onChange={(gender) => this.setState({ gender })} />
-        <MultipleChoice
-          label='Department'
-          required={true}
-          optionsList={[
-            { value: 'Student', checked: false },
-            { value: 'Teacher', checked: false },
-            { value: 'Blahblah', checked: false }
-          ]}
-          message={
-            checkValidate.checkMultipleChoice(this.state.department, true, validations.department)
-          }
-          showMessage={this.state.showMessage}
-          onChange={(department) => this.setState({ department })} />
-        <Input
-          label='Name'
-          required={true}
-          onChange={(name) => { this.setState({ name }) }}
-          message={checkValidate.checkText(this.state.name, validations.name)}
-          showMessage={this.state.showMessage}
+        <SlideTab
+          data={data}
+          handleChange={this.handleChange}
         />
-        <WeekPicker
-          date={this.state.date}
-          handleSelect={this.handleSelect}
-        />
-        <DayPicker
-          date={this.state.date}
-          handleSelect={this.handleSelect}
-        /> */}
       </div>
     )
   }
