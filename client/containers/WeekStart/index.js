8a4bc6f919c753d3c4e0b7ecd56a0f93f160d1c7
zigonk <zthucnguyen99@gmail.com>
2018-04-20 09:28:23 +0700

    weekly start page (reducer is incomplete)

--- client/containers/WeekStart/index.js
+++ client/containers/WeekStart/index.js
@@ -1,32 +1,105 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import TaskMission from './components/TaskMission'
+import OnBlurTextArea from '../../components/OnBlurTextArea/index.jsx'
+import Mission from './components/Mission'
+import { addWeeklyMission } from './action';
+import { debug } from 'util';
 
-export default class WeekStart extends Component {
+class WeekStart extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      goal: 'Hello',
+      missionList: [{name: 'Hello', description: 'Kakaka'}],
+      taskList: [{name: 'Hello', description: "It'me", mission:''}],
     };
   }
-  _changeTaskName = (value) => {
+  _changeTaskName = (value, index) => {
     alert(value);
   }
-  _changeMission = (value, index) => {
+  _changeTaskMission = (value, index) => {
     alert(value);
   }
   _changeDescription = (value, index) => {
     alert(value);
   }
+  _changeMissionName = (value, index) => {
+    alert(value);
+  }
+  _addMission = () => {
+    
+  }
+  _addTask = () => {
+    
+  }
   render() {
+    //const { goal, missionList, taskList } = this.props.weeklyData;
+    debugger
+    const { missions } = this.props.weeklyData;
+    const { _addMission,  _changeMissionName } = this.props;
+    const { goal, taskList } = this.state;
     return (
-      <TaskMission
-        task={{name: 'Hello', description: 'It"s me'}}
-        missionList={[{name: 'BB', description: 'bbbb'}, {name: 'CC', description: 'cccc'}]}
-        changeTaskName={this._changeTaskName}
-        changeMission={this._changeMission}
-        changeDescription={this._changeDescription}
-      />
+      <div>
+        <h1>Make plan for your week</h1>
+        <OnBlurTextArea
+          label="Set your goal"
+          default={"test"}
+          numRows={4}
+        />
+        <div>
+          {
+            missionList && missionList.map((mission, index) => {
+              return (
+                <Mission
+                  index={index}
+                  mission={mission}
+                  changeMissionName={_changeMissionName}
+                  changeMissionDescription={this._changeMissionDescription}
+                />
+              );
+            })
+          }
+          <button onClick={_addMission}>Add Mission</button>
+        </div>
+        <div>
+          {
+            taskList && taskList.map((task, index) => {
+              return (
+                <TaskMission
+                task={task}
+                missionList={missionList}
+                changeTaskName={this._changeTaskName}
+                changeMission={this._changeMission}
+                changeDescription={this._changeDescription}
+                />
+              );
+            })
+          }
+          <button onClick={this._addTask}>Add Task</button>
+        </div>
+      </div>
     );
   }
 }
+
+const mapStateToProps = (state) => {
+   return {
+     weeklyData: state.weeklyData,
+   }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    _addMission: () => dispatch(addWeeklyMission),
+    _changeMissionName: () => dispatch(changeMissionName(value, index)),
+  }
+}
+
+
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(WeekStart);
--- client/store.js
+++ client/store.js
@@ -1,8 +1,10 @@
 import { createStore } from 'redux'
 import todoApp from './reducers/reducers'
+import weeklyStart from './containers/WeekStart/reducer'
  
 const store = createStore(
   todoApp,
+  weeklyStart,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
